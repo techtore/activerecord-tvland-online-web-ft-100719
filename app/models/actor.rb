7008f7958731c4be30ba73a9853bf5de7803281f
Victoria Hale <vic.hale74@gmail.com>
2019-11-12 21:37:37 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
-  has_many :network
+  has_many :networks
 
   def full_name
     "#{self.first_name} #{ self.last_name}"
